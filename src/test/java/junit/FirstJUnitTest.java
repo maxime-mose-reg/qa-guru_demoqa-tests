5ad50fb5b662056ab44c19d263f5ae64bb435bcd
maxime-mose <maxime.mose.reg@gmail.com>
2022-03-19 18:00:14 +0300

    Add 'FirstJUnitTest'

---
+++ src/test/java/junit/FirstJUnitTest.java
@@ -0,0 +1,36 @@
+package junit;
+
+import org.junit.jupiter.api.*;
+
+public class FirstJUnitTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Этот метод выполняется перед всеми тестами.");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("Этот метод выполняется после всех тестов.");
+    }
+
+    @BeforeEach
+    void before() {
+        System.out.println("    Этот метод выполняется перед каждым тестом.");
+    }
+
+    @AfterEach
+    void after() {
+        System.out.println("    Этот метод выполняется после каждого теста.");
+    }
+
+    @Test
+    void firstTest() {
+        System.out.println("        Этот первый тест.");
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("        Этот второй тест.");
+    }
+}
